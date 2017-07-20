7db7ac8798974c4ad66b3d6aca2c0d8f434a53bc
deGeneral <grandious@mail.ru>
2017-07-20 18:20:55 +0300

    Event handler OnClick linked to MainActivity object

--- p0102activitylistener/src/main/java/ru/startandroid/p0102activitylistener/MainActivity.java
+++ p0102activitylistener/src/main/java/ru/startandroid/p0102activitylistener/MainActivity.java
@@ -2,12 +2,42 @@ package ru.startandroid.p0102activitylistener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    TextView tvOut;
+    Button btnOk, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                break;
+        }
     }
 }
