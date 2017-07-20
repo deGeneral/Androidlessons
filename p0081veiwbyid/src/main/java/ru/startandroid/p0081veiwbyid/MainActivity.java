2e2912d8ba0b64d55c35cbd8a9e64eae9cd4b878
deGeneral <grandious@mail.ru>
2017-07-20 17:09:19 +0300

    Button and CheckBox added, their properties changed

--- p0081veiwbyid/src/main/java/ru/startandroid/p0081veiwbyid/MainActivity.java
+++ p0081veiwbyid/src/main/java/ru/startandroid/p0081veiwbyid/MainActivity.java
@@ -2,12 +2,27 @@ package ru.startandroid.p0081veiwbyid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("A new text for my TextView");
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My Button");
+        myBtn.setEnabled(false);
+
+        CheckBox myCheckBox = (CheckBox) findViewById(R.id.myCheckBox);
+        myCheckBox.setChecked(true);
     }
 }
