2ef374e5d15e3a4869301d8fa347689d8931e1b6
deGeneral <grandious@mail.ru>
2017-07-26 19:02:27 +0300

    LogCat records and messages are created

--- p0121logandmes/src/main/java/ru/startandroid/p0121logandmes/MainActivity.java
+++ p0121logandmes/src/main/java/ru/startandroid/p0121logandmes/MainActivity.java
@@ -1,13 +1,52 @@
 package ru.startandroid.p0121logandmes;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "найдем View-элементы");
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        Log.d(TAG, "присваиваем обработчик кнопкам");
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик");
+        switch (v.getId()) {
+            case R.id.btnOk:
+                Log.d(TAG, "кнопка Ok");
+                tvOut.setText("Нажата кнопка ОК");
+                Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCancel:
+                Log.d(TAG, "кнопка Cancel");
+                tvOut.setText("Нажата кнопка Cancel");
+                Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
