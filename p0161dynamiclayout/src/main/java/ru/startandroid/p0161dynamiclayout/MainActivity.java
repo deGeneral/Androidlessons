e4f58cbba3cede0ebee3763cb23252b60d19e5a1
deGeneral <grandious@mail.ru>
2017-08-01 19:22:53 +0300

    Button and TextView added.

--- p0161dynamiclayout/src/main/java/ru/startandroid/p0161dynamiclayout/MainActivity.java
+++ p0161dynamiclayout/src/main/java/ru/startandroid/p0161dynamiclayout/MainActivity.java
@@ -2,18 +2,31 @@ package ru.startandroid.p0161dynamiclayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         LinearLayout linLayout = new LinearLayout(this);
         linLayout.setOrientation(LinearLayout.VERTICAL);
         LayoutParams linLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT,
                 LayoutParams.MATCH_PARENT);
         setContentView(linLayout, linLayoutParams);
+
+        LayoutParams lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
     }
 }
